ad0a5b76dc70f051c8f6bf38b5274c604591df29
Avi Yashchin <3144839+aviyashchin@users.noreply.github.com>
2025-02-02 13:24:37 +0000

    feature: refactoring

--- js/scenes/WinScene.js
+++ js/scenes/WinScene.js
@@ -1,4 +1,4 @@
-import Phaser from 'https://cdn.jsdelivr.net/npm/phaser@3.55.2/dist/phaser.js';
+import Phaser from 'https://unpkg.com/phaser@3.55.2/dist/phaser.module.js';
 import { GAME_WIDTH, GAME_HEIGHT } from '../helpers.js';
 
 export default class WinScene extends Phaser.Scene {
